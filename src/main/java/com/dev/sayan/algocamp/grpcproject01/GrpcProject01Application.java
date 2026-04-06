ecabd0153b3f1e3e9760539454bf830cfca36afd
sayan4545 <sayanchatterjee3010@gmail.com>
2026-04-07 04:56:25 +0530

    Initial set up of the project. added web,data and gRPC server and clent dependencies along with lombok and devtools. created a demo class message.proto file

---
+++ src/main/java/com/dev/sayan/algocamp/grpcproject01/GrpcProject01Application.java
@@ -0,0 +1,13 @@
+package com.dev.sayan.algocamp.grpcproject01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrpcProject01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrpcProject01Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/dev/sayan/algocamp/grpcproject01/GrpcProject01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.sayan.algocamp.grpcproject01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrpcProject01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
