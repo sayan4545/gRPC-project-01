474242f7d757b583d8b736c4ef93b2eab614db3b
sayan4545 <sayanchatterjee3010@gmail.com>
2026-04-24 00:26:10 +0530

    created first proto class

--- src/main/java/com/dev/sayan/algocamp/grpcproject01/GrpcProject01Application.java
+++ src/main/java/com/dev/sayan/algocamp/grpcproject01/GrpcProject01Application.java
@@ -1,13 +1,18 @@
 package com.dev.sayan.algocamp.grpcproject01;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class GrpcProject01Application {
 
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.configure().load();
+        dotenv.entries()
+                        .forEach(entry->System.setProperty(entry.getKey(),entry.getValue()));
         SpringApplication.run(GrpcProject01Application.class, args);
     }
-
 }
